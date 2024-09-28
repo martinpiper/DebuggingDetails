a7861d14a2b700f14272d2986c2b19ed731aa553
Martin Piper <martin.piper@gmail.com>
2024-09-28 22:02:31 +0800

    HTWASM

--- HTWASM/BuildIt.bat
+++ HTWASM/BuildIt.bat
@@ -1,9 +1,9 @@
 @echo off
 del HTWAM.prg
 
-..\..\C64\acme.exe --lib ..\..\C64\ -v4 --msvc HTWAM1.a
+..\..\C64\acme.exe --lib ..\..\C64\ -v4 --msvc HTWAM4.a
 if not exist HTWAM.prg goto error
-..\..\C64\bin\LZMPi.exe -c64mu HTWAM.prg HTWAM.prg $400 >t.txt
+..\..\C64\bin\LZMPi.exe -c64mu HTWAM.prg HTWAM.prg $800 >t.txt
 if not exist HTWAM.prg goto error
 goto end
 :error
