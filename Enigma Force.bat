55cfb444c11cc273be0654d8566940e317dce494
Martin Piper <martin.piper@gmail.com>
2025-03-07 20:38:26 +0800

    Adding a bounding box for the pointer and panel movement

---
+++ Enigma Force.bat
@@ -0,0 +1,3 @@
+..\c64\acme.exe --lib ..\C64\ --msvc -v9 "Enigma Force.a"
+c:\work\c64\bin\LZMPi.exe -pp $37 -c64mbu "Enigma Force 4000.prg" EnigmaForceMouse1.prg $4000
+C:\Downloads\WinVICE-3.1-x86-r34062\WinVICE-3.1-x86-r34062\x64sc.exe -controlport1device 3 -remotemonitor EnigmaForceMouse1.prg
