bc68a0a4abc41852692255b5629d4b9b58b12aea
Martin Piper <martin.piper@gmail.com>
2025-11-17 13:08:37 +0800

    And make the tape version display loading errors

--- CyberdyneWarrior/BuildIt.bat
+++ CyberdyneWarrior/BuildIt.bat
@@ -1,5 +1,5 @@
 @echo off
-del Picture1.prg Picture2.prg Intro.prg Game.prg cyberdyne.tap
+del Picture1.prg Picture2.prg Picture3.prg Intro.prg Game.prg cyberdyne.tap
 
 set COMPOPT=-c64mbu
 set COMMONLIB=--lib ..\..\C64\ --lib ..\..\c64\IRQTape\TapeTool\ --lib ..\..\c64\IRQTape\ -v4 --msvc
@@ -39,12 +39,18 @@ echo Tape file generation
 rem Assemble with tape support
 echo !source "CommonLoadCallTape.a" >CommonLoadCall.a
 
-..\..\C64\acme.exe %COMMONLIB% TapeOptions.a TapeOptionsMessages.a Loaders.a
+del Loaders.bin Picture1.prg Picture2.prg Picture3.prg Intro.prg
 
+..\..\C64\acme.exe %COMMONLIB% TapeOptions.a TapeOptionsMessages.a Loaders.a
+if not exist Loaders.bin goto error
 ..\..\C64\acme.exe %COMMONLIB% -f cbm -o Picture1.prg PictureLink.a Picture1.a
+if not exist Picture1.prg goto error
 ..\..\C64\acme.exe %COMMONLIB% -f cbm -o Picture2.prg PictureLink.a Picture2.a
+if not exist Picture2.prg goto error
 ..\..\C64\acme.exe %COMMONLIB% -f cbm -o Picture3.prg PictureLink.a Picture3.a
+if not exist Picture3.prg goto error
 ..\..\C64\acme.exe %COMMONLIB% -f cbm -o Intro.prg IntroLink.a
+if not exist Intro.prg goto error
 
 ..\..\C64\bin\LZMPi.exe %COMPOPT% Picture1.prg Picture1.prg $c000
 ..\..\C64\bin\LZMPi.exe %COMPOPT% Picture2.prg Picture2.prg $c000
@@ -60,10 +66,10 @@ rem Writes the first turbo data, the main loader, with the "TinyHeader" option
 rem Write the rest of the file data, without any block compression, all the data files are compressed so there would be no gain
 ..\..\C64\bin\TapeTool.exe n 1 w  "cyberdyne.tap" a m "Loaders.map" otl$c0 otfb "Loaders.bin" 0 .SpriteDataStart .SpriteDataEnd $200 c
 ..\..\C64\bin\TapeTool.exe n 1 w  "cyberdyne.tap" a m "Loaders.map" otl$c0 otfb "Picture1.prg" 1 c
-..\..\C64\bin\TapeTool.exe n 1 w  "cyberdyne.tap" a m "Loaders.map" otl$c0 otfb "Picture2.prg" 1 c
-..\..\C64\bin\TapeTool.exe n 1 w  "cyberdyne.tap" a m "Loaders.map" otl$c0 otfb "Picture3.prg" 1 c
-..\..\C64\bin\TapeTool.exe n 1 w  "cyberdyne.tap" a m "Loaders.map" otl$c0 otfb "Intro.prg" 1 c
-..\..\C64\bin\TapeTool.exe n 1 w  "cyberdyne.tap" a m "Loaders.map" otl$c0 otfb "Game.prg" 1 c
+..\..\C64\bin\TapeTool.exe n 1 w  "cyberdyne.tap" a m "Loaders.map" otl$c0 otfb "Picture2.prg" 2 c
+..\..\C64\bin\TapeTool.exe n 1 w  "cyberdyne.tap" a m "Loaders.map" otl$c0 otfb "Picture3.prg" 3 c
+..\..\C64\bin\TapeTool.exe n 1 w  "cyberdyne.tap" a m "Loaders.map" otl$c0 otfb "Intro.prg" 4 c
+..\..\C64\bin\TapeTool.exe n 1 w  "cyberdyne.tap" a m "Loaders.map" otl$c0 otfb "Game.prg" 5 c
 rem Writes a final lead out at the end of the tape
 ..\..\C64\bin\TapeTool.exe n 1 w  "cyberdyne.tap" a m "Loaders.map" otl$c0 c
 
